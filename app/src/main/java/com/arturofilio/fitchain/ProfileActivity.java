71d4d4babfdf77977936848185bc9d3b46a71782
Arturo Filio Villa <arturfil@hotmail.com>
2018-09-18 04:06:26 -0400

    added logout function and image selector, missing saving it in firebase database

--- app/src/main/java/com/arturofilio/fitchain/ProfileActivity.java
+++ app/src/main/java/com/arturofilio/fitchain/ProfileActivity.java
@@ -1,29 +1,167 @@
 package com.arturofilio.fitchain;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.arturofilio.fitchain.Utils.BottomNavigationViewHelper;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private static final String TAG = "ProfileActivity";
 
+    private static final int CHOOSE_IMAGE = 101;
+
     private static final int ACTIVITY_NUM = 2;
     private Context mContext = ProfileActivity.this;
+
+    CircleImageView mProfilePicture;
+    Button mLogoutBtn;
+
+    Uri uriProfileImage;
+    String profileImageUrl;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        mAuth = FirebaseAuth.getInstance();
+
         setupBottomNavigationView();
+
+        mProfilePicture = findViewById(R.id.profile_picture);
+        mLogoutBtn = findViewById(R.id.logout_btn);
+
+        mProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openGallery();
+            }
+        });
+
+        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(mContext, SignInActivity.class);
+                Toast.makeText(mContext, "You were logged out", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Profile Image"), CHOOSE_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == RESULT_OK && data != null && data.getData() != null) {
+            // the user has successfully picked a picture for his new profile
+            // we need to save its reference to a Uri variable
+            uriProfileImage = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uriProfileImage);
+                mProfilePicture.setImageBitmap(bitmap);
+
+                uploadImage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+    private void uploadImage() {
+        StorageReference profileImageReg =
+                FirebaseStorage.getInstance().getReference("profilepics/"+System.currentTimeMillis() + ".jpg");
+
+        if(uriProfileImage != null) {
+            profileImageReg.putFile(uriProfileImage)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(mContext, "Uploading was successful!", Toast.LENGTH_SHORT).show();
+                            profileImageUrl = taskSnapshot.getDownloadUrl().toString();
+                            saveUserInformation();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(mContext, "Uploading Failed", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+        }
+
+
+    }
+
+    private void saveUserInformation() {
+        // TODO: add name and other body stats.
+
+        FirebaseUser user  = mAuth.getCurrentUser();
+
+        if(user != null && profileImageUrl != null) {
+            UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder()
+                    .setPhotoUri(Uri.parse(profileImageUrl))
+                    .build();
+
+            user.updateProfile(profile)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(mContext, "Profile updated!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 
     /**
